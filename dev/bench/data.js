dc29bbf669dbcae350fd950871bad1590c4eddcf
github-action-benchmark <github@users.noreply.github.com>
2020-02-07 09:45:01 +0000

    add HEX parsing (benchmarkjs) benchmark result for feb3b2c2740eafab82ff9cd948a08385338ce17a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1581059249768,
+  "lastUpdate": 1581068701056,
   "repoUrl": "https://github.com/kurkle/color",
   "entries": {
     "Benchmark": [
@@ -59,6 +59,67 @@ window.BENCHMARK_DATA = {
             "range": "±1.21%",
             "unit": "ops/sec",
             "extra": "89 samples"
+          }
+        ]
+      }
+    ],
+    "HEX parsing": [
+      {
+        "commit": {
+          "author": {
+            "email": "jukka.kurkela@gmail.com",
+            "name": "Jukka Kurkela",
+            "username": "kurkle"
+          },
+          "committer": {
+            "email": "jukka.kurkela@gmail.com",
+            "name": "Jukka Kurkela",
+            "username": "kurkle"
+          },
+          "distinct": true,
+          "id": "feb3b2c2740eafab82ff9cd948a08385338ce17a",
+          "message": "Benchmark comparison",
+          "timestamp": "2020-02-07T11:41:57+02:00",
+          "tree_id": "4fe73ed45f36bbbd8993251712b8095fa7a57571",
+          "url": "https://github.com/kurkle/color/commit/feb3b2c2740eafab82ff9cd948a08385338ce17a"
+        },
+        "date": 1581068700633,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "parseInt +",
+            "value": 4127538,
+            "range": "±0.73%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
+          },
+          {
+            "name": "parseInt slice",
+            "value": 3305683,
+            "range": "±0.90%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          },
+          {
+            "name": "map",
+            "value": 12797277,
+            "range": "±0.73%",
+            "unit": "ops/sec",
+            "extra": "91 samples"
+          },
+          {
+            "name": "map obj",
+            "value": 12956488,
+            "range": "±0.49%",
+            "unit": "ops/sec",
+            "extra": "91 samples"
+          },
+          {
+            "name": "parseInt + shift",
+            "value": 10862848,
+            "range": "±0.72%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
           }
         ]
       }
