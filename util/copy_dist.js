e550e48c9c7f9ad29b88dc7bb89928c04938ad90
kurkle <jukka.kurkela@gmail.com>
2020-04-30 10:36:06 +0300

    Update copy_dist.js

--- util/copy_dist.js
+++ util/copy_dist.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 
+if (!fs.existsSync('dist')) {
+	fs.mkdirSync('dist');
+}
+
 fs.copyFile('color.d.ts', 'dist/color.d.ts', (err) => {
 	if (err) {
+		console.warn('working dir: ' + process.cwd());
 		throw err;
 	}
 });
